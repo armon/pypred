5ca66c7cef6e6efbd8ec335f7b9f09c56420a9cc
Armon Dadgar <armon.dadgar@gmail.com>
2013-02-07 12:20:21 -0800

    First pass at lexer

---
+++ pypred/parser.py
@@ -0,0 +1,92 @@
+"""
+This module makes use of the Python PLY package
+to parse the grammars
+"""
+import ply.lex as lex
+
+reserved = {
+    'is': 'IS_EQUALS',
+    'and': 'AND',
+    'or': 'OR',
+    'not': 'NOT',
+    'contains': 'CONTAINS',
+    'matches': 'MATCHES',
+    'true': 'TRUE',
+    'false': 'FALSE',
+    'undefined': 'UNDEFINED',
+    'null': 'NULL'
+}
+
+tokens = (
+    'AND',
+    'OR',
+    'NOT',
+    'GREATER_THAN',
+    'GREATER_THAN_EQUALS',
+    'LESS_THAN',
+    'LESS_THAN_EQUALS',
+    'EQUALS',
+    'NOT_EQUALS',
+    'IS_EQUALS',
+    'IS_NOT_EQUALS',
+    'LPAREN',
+    'RPAREN',
+    'CONTAINS',
+    'MATCHES',
+    'NUMBER',
+    'STRING',
+    'TRUE',
+    'FALSE',
+    'UNDEFINED',
+    'NULL'
+)
+
+# Regex rules for tokens
+t_GREATER_THAN = r'>'
+t_GREATER_THAN_EQUALS = r'>='
+
+t_LESS_THAN = r'<'
+t_LESS_THAN_EQUALS = r'<='
+
+t_EQUALS = r'='
+t_NOT_EQUALS = r'!='
+
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+
+# Ignore any comments
+t_ignore_COMMENT = r'\#.*'
+
+def t_NUMBER(t):
+    r'-?\d+(\.\d+)?'
+    t.value = float(t.value)
+    return t
+
+# Matches either a sequence of non-whitespace
+# or anything that is quoted
+def t_STRING(t):
+    r'([\w_\-.:;]+|"[^"]*"|\'[^\']*\')'
+    # Check for reserved words
+    t.type = reserved.get(t.value,'STRING')
+    t.value = t.value.strip("\"'")
+    return t
+
+# Track the newlines
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# The ignore characters
+t_ignore = ' \t\n'
+
+# Error handler
+def t_error(t):
+    error_loc = (t.value, t.lexer.lexpos, t.lexer.lineno)
+    t.lexer.errors.append(error_loc)
+    t.lexer.skip(1)
+
+# Build the lexer
+def get_lexer():
+    "Returns a new instance of the lexer"
+    return lex.lex()
+
