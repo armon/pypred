84e19f5b5e4275162d3cd074e5bb2043e7de5447
Armon Dadgar <armon.dadgar@gmail.com>
2013-02-08 15:44:46 -0800

    Change __init__ so that setup.py works

--- pypred/__init__.py
+++ pypred/__init__.py
@@ -1,7 +1,11 @@
-from parser import get_lexer, get_parser
-import ast
-from predicate import Predicate, InvalidPredicate
-
-__all__ = ["get_lexer", "get_parser", "ast", "Predicate", "InvalidPredicate"]
 __version__ = "0.1.0"
+
+try:
+    from parser import get_lexer, get_parser
+    import ast
+    from predicate import Predicate, InvalidPredicate
+    __all__ = ["get_lexer", "get_parser", "ast", "Predicate", "InvalidPredicate"]
+except ImportError:
+    print "Missing dependencies!"
+    __all__ = []
 
