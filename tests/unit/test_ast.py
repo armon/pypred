4ce3ea3d67b38404bfe0094072fc461674b0c35c
Armon Dadgar <armon.dadgar@gmail.com>
2013-02-08 13:51:48 -0800

    Testing comparison

--- tests/unit/test_ast.py
+++ tests/unit/test_ast.py
@@ -198,3 +198,25 @@ class TestAST(object):
         assert info["literals"]["l"] == d["l"]
         assert info["literals"]["r"] == d["r"]
 
+    @pytest.mark.parametrize(("type",), [
+        (">=",), (">",), ("<",), ("<=",), ("=",), ("!=",), ("is",)])
+    def test_compare_undef(self, type):
+        l = ast.Literal("l")
+        r = ast.Literal("r")
+        a = ast.CompareOperator(type, l, r)
+        d = {"l": 1}
+        res, info = a.analyze(MockPred(), d)
+
+        # Determine the expected result
+        if type == "=":
+            s = '%d %s %d' % (d["l"], "==", d["r"])
+        else:
+            s = '%d %s %d' % (d["l"], type, d["r"])
+        expected = eval(s)
+
+        assert res == expected
+        if not res:
+            assert ("%s comparison at" % type.upper()) in info["failed"][0]
+        assert info["literals"]["l"] == d["l"]
+        assert info["literals"]["r"] == ast.Undefined()
+
