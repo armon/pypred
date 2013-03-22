0fbb25b6a8e40c31f6c75ccd06f9073724b75cc8
Armon Dadgar <armon.dadgar@gmail.com>
2013-03-21 18:20:18 -0700

    Export optimized set

--- pypred/__init__.py
+++ pypred/__init__.py
@@ -3,6 +3,7 @@ __version__ = "0.2.0"
 from parser import get_lexer, get_parser
 import ast
 from predicate import Predicate, InvalidPredicate
-from set import PredicateSet
-__all__ = ["get_lexer", "get_parser", "ast", "Predicate", "InvalidPredicate"]
+from set import PredicateSet, OptimizedPredicateSet
+__all__ = ["get_lexer", "get_parser", "ast", "Predicate",
+"InvalidPredicate", "PredicateSet", "OptimizedPredicateSet"]
 
