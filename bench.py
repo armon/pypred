9a20a48a01b7e94354846fedb35980bd6edf1c81
Armon Dadgar <armon.dadgar@gmail.com>
2013-03-21 18:20:38 -0700

    Improving bench tool

--- bench.py
+++ bench.py
@@ -1,6 +1,7 @@
 import time
 import random
-from pypred import Predicate, PredicateSet
+random.seed(1)
+from pypred import Predicate, PredicateSet, OptimizedPredicateSet
 
 def get_words():
     "Returns a large list of words"
@@ -32,7 +33,7 @@ def timed(f):
 def gen_predicates(num):
     res = []
     for x in xrange(num):
-        r = random.randint(0,4)
+        r = random.randint(0,5)
         if r == 0:
             p_str = "name is '%s'" % random.choice(SELECT_WORDS)
         elif r == 1:
@@ -50,6 +51,13 @@ def gen_predicates(num):
         elif r == 4:
             gender = "Male" if random.random() else "Female"
             p_str = "name is '%s' or gender is '%s'" % (random.choice(SELECT_WORDS), gender)
+        elif r == 5:
+            gender = "Male" if random.random() else "Female"
+            age = random.randint(1, 100)
+            p_str = "(age > %d and gender is '%s')" % (age, gender)
+            gender = "Male" if random.random() else "Female"
+            age = random.randint(1, 100)
+            p_str += " or (age < %d and gender is '%s')" % (age, gender)
 
         p = Predicate(p_str)
         res.append(p)
@@ -71,23 +79,57 @@ def gen_docs(num):
 def make_set(preds):
     return PredicateSet(preds)
 
+@timed
+def make_set_optimized(preds):
+    s = OptimizedPredicateSet(preds)
+    s.compile_ast()
+    #print s.ast.description()
+    return s
+
 def main(numpreds=100, numdocs=2000, printp=0):
     preds = gen_predicates(numpreds)
     docs = gen_docs(numdocs)
-    s = make_set(preds)
+    s1 = make_set(preds)
+    s2 = make_set_optimized(preds)
 
     if printp:
+        print "Predicates:"
         for p in preds:
-            print p.predicate
+            print "\t", p.predicate
 
     # Time the evaluation
     start = time.time()
     total = 0
     for d in docs:
-        total += len(s.evaluate(d))
+        total += len(s1.evaluate(d))
     end = time.time()
-    print "Evaluated %d docs across %d predicates in %0.3f seconds" % (numdocs, numpreds, end-start)
-    print "Total of %d predicates matched" % total
+    print "(Naive) Evaluated %d docs across %d predicates in %0.3f seconds" % (numdocs, numpreds, end-start)
+    print "(Naive) Total of %d predicates matched" % total
+
+    # Time the evaluation
+    start = time.time()
+    total_o = 0
+    for d in docs:
+        total_o += len(s2.evaluate(d))
+    end = time.time()
+    print "(Opt) Evaluated %d docs across %d predicates in %0.3f seconds" % (numdocs, numpreds, end-start)
+    print "(Opt) Total of %d predicates matched" % total_o
+
+    if total != total_o:
+        print "Mismatch! Differing inputs:"
+        for d in docs:
+            r1 = s1.evaluate(d)
+            r2 = s2.evaluate(d)
+            if r1 != r2:
+                print "Input:",repr(d)
+                print "Naive:"
+                for p in r1:
+                    print "\t",p.predicate
+                print "Opt:"
+                for p in r2:
+                    print "\t",p.predicate
+                print
+
 
 
 if __name__ == "__main__":
