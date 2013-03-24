0721aab846dcfd1301ff36a5768bae0d2a6a11e1
Armon Dadgar <armon.dadgar@gmail.com>
2013-03-23 17:02:57 -0700

    Expose settings

--- pypred/__init__.py
+++ pypred/__init__.py
@@ -1,9 +1,14 @@
 __version__ = "0.2.0"
 
-from parser import get_lexer, get_parser
-import ast
+from merge import RefactorSettings
 from predicate import Predicate, InvalidPredicate
 from set import PredicateSet, OptimizedPredicateSet
-__all__ = ["get_lexer", "get_parser", "ast", "Predicate",
-"InvalidPredicate", "PredicateSet", "OptimizedPredicateSet"]
+
+__all__ = [
+    "Predicate",
+    "InvalidPredicate",
+    "PredicateSet",
+    "OptimizedPredicateSet",
+    "RefactorSettings"
+]
 
