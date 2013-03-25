ad3282944fd63bfcc5640eac1e167acbcce9081b
Armon Dadgar <armon.dadgar@gmail.com>
2013-03-25 10:19:23 -0700

    More tests

--- tests/unit/test_merge.py
+++ tests/unit/test_merge.py
@@ -1,3 +1,4 @@
+from mock import patch
 from pypred import predicate, merge, ast
 
 class TestMerge(object):
@@ -50,4 +51,79 @@ name matches '^test$' and list contains elem and foo > 20
         k = ("CompareOperator", "order", ("Literal", "foo"), ("Number", "static"))
         assert k in names
         assert count[k] == 2
+
+    def test_rewrite_norm(self):
+        "Test a simple re-write"
+        l = ast.Literal('foo')
+        r = ast.Literal('bar')
+        c = ast.LogicalOperator('or', l, r)
+        name = merge.node_name(c)
+        merge.rewrite_ast(c, name, l, True)
+        assert isinstance(c.left, ast.Constant)
+
+    def test_rewrite_compare(self):
+        "Checks that a compare rewrite uses compare module"
+        l = ast.Literal('foo')
+        r = ast.Literal('bar')
+        n = ast.CompareOperator('>', l, r)
+        name = merge.node_name(n)
+        with patch('pypred.merge.compare.compare_rewrite') as c:
+            merge.rewrite_ast(n, name, n, True)
+            assert c.called
+
+    def test_select_expr_negate(self):
+        "Checks that a negate operation is not selected"
+        l = ast.Literal('foo')
+        n = ast.NegateOperator(l)
+        name = merge.node_name(n)
+        expr = merge.select_rewrite_expression(name, [n])
+        assert expr == l
+
+    def test_select_expr_logical(self):
+        "Checks that a logical operation is not selected"
+        l = ast.Literal('foo')
+        r = ast.Literal('bar')
+        n = ast.LogicalOperator('or', l, r)
+        name = merge.node_name(n)
+        expr = merge.select_rewrite_expression(name, [n])
+        assert expr == l
+        n.left = None
+        expr = merge.select_rewrite_expression(name, [n])
+        assert expr == r
+
+    def test_select_expr_first(self):
+        "Checks that first expression is selected"
+        l = ast.Literal('foo')
+        r = ast.Literal('bar')
+        name = merge.node_name(l)
+        expr = merge.select_rewrite_expression(name, [l, r])
+        assert expr == l
+
+    def test_select_expr_compare(self):
+        "Checks that a compare operation uses compare module"
+        l = ast.Literal('foo')
+        r = ast.Literal('bar')
+        n = ast.CompareOperator('>', l, r)
+        name = merge.node_name(n)
+
+        with patch('pypred.merge.compare.select_rewrite_expression') as c:
+            merge.select_rewrite_expression(name, [n])
+            assert c.called
+
+    def test_merge(self):
+        "Tests a simple merge"
+        p1 = predicate.Predicate('foo')
+        p2 = predicate.Predicate('bar')
+        p3 = predicate.Predicate('baz')
+        p4 = predicate.Predicate('zip')
+        m = merge.merge([p1,p2,p3,p4])
+        assert isinstance(m, ast.Both)
+        assert isinstance(m.left, ast.Both)
+        assert isinstance(m.right, ast.Both)
+        assert m.left.left.pred    == p1
+        assert m.left.right.pred   == p2
+        assert m.right.left.pred   == p3
+        assert m.right.right.pred  == p4
+
+
 
