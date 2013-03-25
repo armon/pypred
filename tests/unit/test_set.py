62d5a4446c4c0a919557dd5f2e95d21c5a8259a8
Armon Dadgar <armon.dadgar@gmail.com>
2013-03-25 15:17:50 -0700

    Test the optimized set

--- tests/unit/test_set.py
+++ tests/unit/test_set.py
@@ -1,4 +1,5 @@
-from pypred import PredicateSet, Predicate
+import pytest
+from pypred import OptimizedPredicateSet, PredicateSet, Predicate
 
 class TestPredicateSet(object):
     def test_two(self):
@@ -13,4 +14,44 @@ class TestPredicateSet(object):
         s = PredicateSet([p1, p1])
         match = s.evaluate({'name': 'Jill'})
         assert match == [p1]
+
+class TestOptPredicateSet(object):
+    def test_two(self):
+        p1 = Predicate("name is 'Jack'")
+        p2 = Predicate("name is 'Jill'")
+        s = OptimizedPredicateSet([p1, p2])
+        match = s.evaluate({'name': 'Jill'})
+        assert match == [p2]
+
+    def test_dup(self):
+        p1 = Predicate("name is 'Jill'")
+        s = OptimizedPredicateSet([p1, p1])
+        match = s.evaluate({'name': 'Jill'})
+        assert match == [p1]
+
+    def test_invalidate(self):
+        "AST is invalidated when set changes"
+        p1 = Predicate("name is 'Jack'")
+        p2 = Predicate("name is 'Jill'")
+        s = OptimizedPredicateSet([p1, p2])
+        match = s.evaluate({'name': 'Jill'})
+        assert match == [p2]
+
+        p3 = Predicate("name is 'Joe'")
+        s.add(p3)
+        assert s.ast == None
+        match = s.evaluate({'name': 'Joe'})
+        assert match == [p3]
+
+    def test_finalize(self):
+        p1 = Predicate("name is 'Jack'")
+        p2 = Predicate("name is 'Jill'")
+        s = OptimizedPredicateSet([p1, p2])
+        s.finalize()
+        match = s.evaluate({'name': 'Jill'})
+        assert match == [p2]
+
+        p3 = Predicate("name is 'Joe'")
+        with pytest.raises(Exception):
+            s.add(p3)
 
